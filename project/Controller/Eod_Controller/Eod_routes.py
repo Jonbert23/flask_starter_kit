88ef7c7c2f9f9fcd59c512442eb22252a14a03bb
unknown <jonbertandam@gmai.com>
2022-07-01 08:03:49 +0800

    Adding New Logo

--- project/Controller/Eod_Controller/Eod_routes.py
+++ project/Controller/Eod_Controller/Eod_routes.py
@@ -3,6 +3,6 @@ from flask_login import login_required, current_user
 
 eod = Blueprint('eod', __name__)
 
-@eod.route("/eod")
+@eod.route("/eod-form")
 def eodForm():
     return render_template('Eod_Template/Eod_index.html')
