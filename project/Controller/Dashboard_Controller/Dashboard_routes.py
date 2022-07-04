34dd27de836138a70fd5975e4cb404b715817161
roypaul21 <roypaul.narido@g.msuiit.edu.ph>
2022-07-04 22:08:52 +0800

    update

--- project/Controller/Dashboard_Controller/Dashboard_routes.py
+++ project/Controller/Dashboard_Controller/Dashboard_routes.py
@@ -1,8 +1,47 @@
 from flask import Blueprint, render_template, url_for, request, redirect
 from flask_login import login_required, current_user
+from .Dashboard_xpath import BreakdownXpath
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 dash = Blueprint('dash', __name__)
 
-@dash.route("/dashboard")
+@login_required
+@dash.route("/dashboard", methods=['POST', 'GET'])
 def dashboard():
+    if request.method == 'POST':
+        url = "https://solo.next.jarvisanalytics.com/solo/results"
+        date_from = "2022-07-01"
+        date_to = "2022-07-31"
+        breakdownTest(date_from, date_to)
+
     return render_template('Dashboard_Template/Dashboard_index.html')
+
+
+def getmetricsXpath(metrics):
+    metric = {}
+
+    for x in metrics:
+        metric[x] = BreakdownXpath.financial_dataxpath[x]
+
+    return metric
+
+
+
+def breakdownTest(date_from, date_to):
+    
+    metrics = ["net_prod", "gross_prod", "collection", "adjustment", "newpatient_visit", "existingpatient_visit"]
+    metricsXpath = getmetricsXpath(metrics)
+
+    if metrics:
+        for metric in metricsXpath:
+            metric_base = metricsXpath[metric][0]
+            metric_brk = metricsXpath[metric][1]
+            metric_brktotal = metricsXpath[metric][2]
+            metric_brkclose = metricsXpath[metric][3]
+
+            print(f"------->{metric_base}")
+            print(f">{metric_brk}")
+            print(f">{metric_brktotal}")
+            print(f">{metric_brkclose}")
---
+++ project/Controller/Dashboard_Controller/Dashboard_xpath.py
@@ -0,0 +1,50 @@
+
+class BreakdownXpath():
+    # --Financial Data
+
+    # Net production
+    netprod_base = "/html/body/div[1]/main/div[2]/section[1]/div[1]/div[1]/div/div/div[2]/h3"
+    netprod_brk = "/html/body/div[1]/main/div[2]/section[1]/div[1]/div[1]/div/div/div[3]/a/ja-"
+    netprod_brktotal = "/html/body/div[1]/main/div[2]/section[1]/div[1]/div[1]/div/div/div[3]/div/div/div/div[2]/div[2]/table/tr/td[3]/span"
+    netprod_brkclose = "/html/body/div[1]/main/div[2]/section[1]/div[1]/div[1]/div/div/div[3]/div/div/div/div[1]/button"
+
+    # Gross production
+    grossprod_base = "/html/body/div[1]/main/div[2]/section[1]/div[1]/div[2]/div/div/div[2]/h3"
+    grossprod_brk = "/html/body/div[1]/main/div[2]/section[1]/div[1]/div[2]/div/div/div[3]/a/ja-"
+    grossprod_brktotal = "/html/body/div[1]/main/div[2]/section[1]/div[1]/div[2]/div/div/div[3]/div/div/div/div[2]/div[2]/table/tr/td[3]/span"
+    grossprod_brkclose = "/html/body/div[1]/main/div[2]/section[1]/div[1]/div[2]/div/div/div[3]/div/div/div/div[1]/button"
+
+    # Collection
+    collection_base = "/html/body/div[1]/main/div[2]/section[1]/div[1]/div[3]/div/div/div[2]/h3"
+    collection_brk = "/html/body/div[1]/main/div[2]/section[1]/div[1]/div[3]/div/div/div[3]/a/ja-"
+    collection_brktotal = "/html/body/div[1]/main/div[2]/section[1]/div[1]/div[3]/div/div/div[3]/div/div/div/div[2]/div[2]/table/tr/td[3]/span"
+    collection_brkclose = "/html/body/div[1]/main/div[2]/section[1]/div[1]/div[3]/div/div/div[3]/div/div/div/div[1]/button"
+
+    # Adjustment
+    adjustment_base = "/html/body/div[1]/main/div[2]/section[1]/div[1]/div[4]/div/div/div[2]/h3"
+    adjustment_brk = "/html/body/div[1]/main/div[2]/section[1]/div[1]/div[4]/div/div/div[3]/a/ja-"
+    adjustment_brktotal = "/html/body/div[1]/main/div[2]/section[1]/div[1]/div[4]/div/div/div[3]/div/div/div/div[2]/div[2]/table/tr/td[3]/span"
+    adjustment_brkclose = "/html/body/div[1]/main/div[2]/section[1]/div[1]/div[4]/div/div/div[3]/div/div/div/div[1]/button"
+
+    # --Patient Data
+
+    # New Patient Visits
+    nvs_base = "/html/body/div[1]/main/div[2]/section[2]/div/div[1]/div/div[1]/div/div/div[2]/h3"
+    nvs_brk = "/html/body/div[1]/main/div[2]/section[2]/div/div[1]/div/div[1]/div/div/div[3]/a/ja-"
+    nvs_brktotal = "/html/body/div[1]/main/div[2]/section[2]/div/div[1]/div/div[1]/div/div/div[3]/div/div/div/div[2]/div[2]/table/tr/td[6]/span"
+    nvs_brkclose = "/html/body/div[1]/main/div[2]/section[2]/div/div[1]/div/div[1]/div/div/div[3]/div/div/div/div[1]/button"
+
+    # Existing Patient Visits
+    evs_base = "/html/body/div[1]/main/div[2]/section[2]/div/div[1]/div/div[2]/div/div/div[2]/h3"
+    evs_brk = "/html/body/div[1]/main/div[2]/section[2]/div/div[1]/div/div[2]/div/div/div[3]/a/ja-"
+    evs_brktotal = "/html/body/div[1]/main/div[2]/section[2]/div/div[1]/div/div[2]/div/div/div[3]/div/div/div/div[2]/div[2]/table/tr/td[5]/span"
+    evs_brkclose = "/html/body/div[1]/main/div[2]/section[2]/div/div[1]/div/div[2]/div/div/div[3]/div/div/div/div[1]/button"
+
+    # all dictionary xpath
+    financial_dataxpath = {"net_prod": [netprod_base, netprod_brk, netprod_brktotal, netprod_brkclose],
+                           "gross_prod": [grossprod_base, grossprod_brk, grossprod_brktotal, grossprod_brkclose],
+                           "adjustment": [adjustment_base, adjustment_brk, adjustment_brktotal, adjustment_brkclose],
+                           "collection": [collection_base, collection_brk, collection_brktotal, collection_brkclose],
+                           "newpatient_visit": [nvs_base, nvs_brk, nvs_brktotal, nvs_brkclose],
+                           "existingpatient_visit": [evs_base, evs_brk, evs_brktotal, evs_brkclose]
+    }
