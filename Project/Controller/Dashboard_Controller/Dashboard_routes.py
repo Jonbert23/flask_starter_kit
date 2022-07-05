95ad9d788b45a9ea64b965d50e88f3171a484812
roypaul21 <roypaul.narido@g.msuiit.edu.ph>
2022-07-05 17:52:48 +0800

    update dashboard

--- Project/Controller/Dashboard_Controller/Dashboard_routes.py
+++ Project/Controller/Dashboard_Controller/Dashboard_routes.py
@@ -4,8 +4,14 @@ from .Dashboard_xpath import BreakdownXpath
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from .Dashboard_tester import testDashboard
+
 import time
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
+from selenium.webdriver.chrome.service import Service
+from selenium import webdriver
+from Project.Controller.Global_Controller.Global_test import Login
+
 
 # --Blueprint
 dash = Blueprint('dash', __name__)
@@ -14,7 +20,14 @@ dash = Blueprint('dash', __name__)
 @dash.route("/dashboard", methods=['POST', 'GET'])
 def dashboard():
     if request.method == 'POST':
-        driver = testDashboard("as")
+        d = DesiredCapabilities.CHROME
+        d['loggingPrefs'] = {'browser': 'ERROR'}
+        driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), desired_capabilities=d)
+        Login.login(driver, "https://solo.next.jarvisanalytics.com/end-of-day", "testryan", "Jarvis.123")
+
+        if not driver:
+            return False
+
         date_from = "2022-07-01"
         date_to = "2022-07-31"
         breakdownTest(driver, date_from, date_to)
@@ -31,34 +44,60 @@ def getmetricsXpath(metrics):
     return metric
 
 # Select Date-Range in Calendar
-def dateFrom():
+def dateFrom(driver):
     pass
 
-def dateTo(date_to):
-    yt, mt, dt = date_to.split('-')
+def dateTo(driver):
+    pass
 
 def setdateCalendar(driver, date_from, date_to):
     yf, mf, df = date_from.split('-')
 
+    yt, mt, dt = date_to.split('-')
+
     print("----Calendar Date----")
 
+    # Open Calendar Date Picker
     time.sleep(5)
-    dateElement = WebDriverWait(driver, 120).until(EC.presence_of_element_located((By.XPATH, f"{BreakdownXpath.date_picker}")))
+    dateElement = WebDriverWait(driver, 1000).until(EC.presence_of_element_located((By.XPATH, f"{BreakdownXpath.date_picker}")))
     dateElement.click()
 
-    month = WebDriverWait(driver, 30).until(
+    # From Date Next and Previous
+    prevMonthFrom = driver.find_element(
+        by=By.XPATH,
+        value=f"{BreakdownXpath.prev_monthfrom}"
+    )
+    nextMonthFrom = driver.find_element(
+        by=By.XPATH,
+        value=f"{BreakdownXpath.next_monthfrom}"
+    )
+
+    prevMonthFrom = driver.find_element(
+        by=By.XPATH,
+        value=f"{BreakdownXpath.prev_monthfrom}"
+    )
+    nextMonthFrom = driver.find_element(
+        by=By.XPATH,
+        value=f"{BreakdownXpath.next_monthfrom}"
+    )
+
+
+    month = WebDriverWait(driver, 1000).until(
         EC.visibility_of_element_located((By.XPATH, f"{BreakdownXpath.curr_monthfrom}"))
     )
     date_month = month.text
 
-    year = WebDriverWait(driver, 30).until(
+    year = WebDriverWait(driver, 1000).until(
         EC.visibility_of_element_located((By.XPATH, f"{BreakdownXpath.curr_yearfrom}"))
     )
     date_year = year.text
 
-    print(date_month)
-    print(date_year)
+    if yf == yt and mf == mt:
+        pass
 
+    else:
+        dateFrom(driver)
+        dateTo(driver)
 
 def breakdownTest(driver, date_from, date_to):
     setdateCalendar(driver, date_from, date_to)
--- Project/Controller/Dashboard_Controller/Dashboard_xpath.py
+++ Project/Controller/Dashboard_Controller/Dashboard_xpath.py
@@ -8,7 +8,10 @@ class BreakdownXpath():
     curr_yearfrom = "/html/body/div[1]/main/div[1]/div/div/div/div[1]/div/div[2]/div/div[2]/div[1]/div/table/thead/tr/th[2]/div/input"
     prev_monthto = "/html/body/div[1]/main/div[1]/div/div/div/div[1]/div/div[2]/div/div[2]/div[2]/div/table/thead/tr/th[1]"
     next_monthto = "/html/body/div[1]/main/div[1]/div/div/div/div[1]/div/div[2]/div/div[2]/div[2]/div/table/thead/tr/th[3]"
-    curr_month_yearto = ""
+    curr_monthto = "/html/body/div[1]/main/div[1]/div/div/div/div[1]/div/div[2]/div/div[2]/div[2]/div/table/thead/tr/th[2]/div/select"
+    curr_yearto = "/html/body/div[1]/main/div[1]/div/div/div/div[1]/div/div[2]/div/div[2]/div[2]/div/table/thead/tr/th[2]/div/input"
+
+    update_btn = "/html/body/div[1]/main/div[1]/div/div/div/div[4]/button"
 
     # --Financial Data
 
